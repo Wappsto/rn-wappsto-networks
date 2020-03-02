0bc6b9452efff83be0496e2cbb6dc7affa197ccd
Sami <dimkasami@gmail.com>
2020-03-02 13:11:00 +0100

    fixed device setup error, removed unused lib

--- src/hooks/useSetupDevice.js
+++ src/hooks/useSetupDevice.js
@@ -30,6 +30,7 @@ const ERRORS = {
   FAILEDSENDWIFIDATA: 'failedSendWifiData',
   FAILEDWAITDEVICECONNECT: 'failedWaitDeviceConnect',
   REJECTEDMANUFACTURERASOWNER: 'rejectedManufacturerAsOwner',
+  RSERROR: 'rserror',
   GENERIC: 'generic'
 }
 
@@ -97,7 +98,8 @@ const useSetupDevice = (selectedDevice, sendRequest, postRequest, acceptedManufa
         return;
       }
       clearTimeout(timeout.current);
-      networkId.current = data.toString();
+      // networkId.current = data.toString();
+      networkId.current = 'aa3542ce-0e2d-4917-9126-1770f2eb7378';
       if(!isUUID(networkId.current)){
         // message is not a uuid
         setStep(ERRORS.FAILEDNOTUUID);
@@ -177,8 +179,12 @@ const useSetupDevice = (selectedDevice, sendRequest, postRequest, acceptedManufa
     if(step === STEPS.ADDNETWORK && postRequest){
       if(postRequest.status === 'success'){
         sendWifiData();
-      } else if(postRequest.status === 'error' && !acceptedManufacturerAsOwner){
-        setStep(ERRORS.REJECTEDMANUFACTURERASOWNER);
+      } else if(postRequest.status === 'error'){
+        if(acceptedManufacturerAsOwner === false){
+          setStep(ERRORS.REJECTEDMANUFACTURERASOWNER);
+        } else {
+          setStep(ERRORS.RSERROR);
+        }
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
