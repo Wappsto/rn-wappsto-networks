c3397cfe2d0364fa5bc76ef6141f3cd0e400b07d
Evelina <evelinamihailova@gmail.com>
2020-03-30 17:02:29 +0200

    added placeholder images for the onboarding flow

---
+++ src/theme/images.js
@@ -0,0 +1,10 @@
+export default {
+	onboarding: {
+    deviceDiscovery: require('../../assets/placeholder.png'),
+    devicesNotFound: require('../../assets/placeholder.png'),
+    wifiConfig: require('../../assets/placeholder.png'),
+    wifiSetup: require('../../assets/placeholder.png'),
+    wifiSetupSuccess: require('../../assets/placeholder.png'),
+		wifiSetupError: require('../../assets/placeholder.png')
+	}
+}
