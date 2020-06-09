82518186e1daa99645532abb8604b953016302e2
Sami <dimkasami@gmail.com>
2020-06-09 17:20:36 +0200

    handle duplicate translations

--- scripts/translation/csv2json.js
+++ scripts/translation/csv2json.js
@@ -7,49 +7,52 @@ const del = require('del');
 const result = {};
 const csvPath = paths.shift();
 
+const projectNameSplitter = '^';
 csv2json()
 .fromFile(csvPath)
 .then((json)=>{
   json.forEach((input) => {
     const key = input.key;
-    let projectName = '.';
+    let projectNames = ['.'];
     let str = key;
-    if(key.startsWith('^')){
-      const split = key.split('^');
-      projectName = split[1];
-      str = split[2];
+    if(key.startsWith(projectNameSplitter)){
+      projectNames = key.split(projectNameSplitter);
+      projectNames.shift()
+      str = projectNames.pop();
     }
     const strSplit = str.split(':');
     const namespace = strSplit[0];
     let tKey = strSplit[1];
-    if(!result[projectName]){
-      result[projectName] = {};
-    }
     tKey = tKey.split('.');
-    for(let language in input){
-      if(language !== 'key'){
-        const value = input[language];
-        if(!result[projectName][language]){
-          result[projectName][language] = {};
-        }
-        if(!result[projectName][language][namespace]){
-          result[projectName][language][namespace] = {};
-        }
-        if(tKey.length > 1){
-          const lastObj = tKey.reduce((obj, k, index) => {
-            if(!obj[k]){
-              obj[k] = {};
-            }
-            if(index === tKey.length - 1){
-              obj[k] = value;
-            }
-            return obj[k];
-          }, result[projectName][language][namespace]);
-        } else {
-          result[projectName][language][namespace][tKey[0]] = value;
+    projectNames.forEach(projectName => {
+      if(!result[projectName]){
+        result[projectName] = {};
+      }
+      for(let language in input){
+        if(language !== 'key'){
+          const value = input[language];
+          if(!result[projectName][language]){
+            result[projectName][language] = {};
+          }
+          if(!result[projectName][language][namespace]){
+            result[projectName][language][namespace] = {};
+          }
+          if(tKey.length > 1){
+            const lastObj = tKey.reduce((obj, k, index) => {
+              if(!obj[k]){
+                obj[k] = {};
+              }
+              if(index === tKey.length - 1){
+                obj[k] = value;
+              }
+              return obj[k];
+            }, result[projectName][language][namespace]);
+          } else {
+            result[projectName][language][namespace][tKey[0]] = value;
+          }
         }
       }
-    }
+    })
   });
   createFiles();
 });
--- scripts/translation/json2csv.js
+++ scripts/translation/json2csv.js
@@ -3,12 +3,13 @@ const Files = require('./files');
 const { parse } = require('json2csv');
 const paths = process.argv.slice(2);
 
+const projectNameSplitter = '^';
 const result = {};
 paths.forEach(path => {
   let projectName = '';
   if(paths.length > 1){
     projectName = path.replace(/src.*$/, '');
-    projectName = '^' + Path.basename(projectName) + '^';
+    projectName = Path.basename(projectName);
   }
   const folders = Files.getAllFolders(path);
   folders.forEach(folderPath => {
@@ -18,24 +19,26 @@ paths.forEach(path => {
     files.forEach(filePath => {
       const name = Path.basename(filePath, '.json');
       const content = Files.loadJsonFile(filePath);
-      addJSON(language, content, projectName + name + ':');
+      addJSON(language, content, name + ':', projectName);
     });
   });
 });
 const csv = parse(Object.values(result));
 Files.saveFile('result.csv', csv);
 
-function addJSON(language, json, prefix){
+function addJSON(language, json, prefix, projectName){
   for(let key in json){
     if(typeof json[key] === 'string'){
       const k = prefix + key;
       const v = json[key];
       if(!result[k]){
-        result[k] = { key: k };
+        result[k] = { key: projectNameSplitter + projectName + projectNameSplitter + k };
+      } else if(!result[k].key.includes(projectName)){
+        result[k].key = projectNameSplitter + projectName + result[k].key;
       }
       result[k][language] = v;
     } else {
-      addJSON(language, json[key], prefix + key + '.');
+      addJSON(language, json[key], prefix + key + '.', projectName);
     }
   }
 }
