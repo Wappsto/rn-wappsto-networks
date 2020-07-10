3696542de2ceec0968745cfb062cdd7afa9a6bee
Evelina <evelinamihailova@gmail.com>
2020-07-10 13:57:48 +0200

    better handling for image overrides

--- src/theme/images.js
+++ src/theme/images.js
@@ -1,6 +1,14 @@
 let Images = {};
 
+let called = false;
 export const useImages = (images = {}) => {
+	if(called){
+		console.log('============================');
+		console.log('= useImages already called =');
+		console.log('============================');
+		return;
+	}
+	called = true;
 	Object.assign(Images, images);
 }
 
