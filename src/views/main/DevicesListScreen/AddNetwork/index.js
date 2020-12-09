34d46b9a2db52c0f7ebeaf8ce40d5cc8b9efc977
Sami <dimkasami@gmail.com>
2020-12-09 16:06:14 +0100

    fixed status bar

--- src/views/main/DevicesListScreen/AddNetwork/index.js
+++ src/views/main/DevicesListScreen/AddNetwork/index.js
@@ -84,13 +84,13 @@ const Content = React.memo(({ visible, hide, show }) => {
 
   return (
     <>
-      <StatusBar backgroundColor={theme.variables.statusBarBgLight} barStyle={theme.variables.statusBarColorDark} />
       <Modal
         transparent={true}
         visible={visible}
         hide={hide}
         onRequestClose={handleBack}>
           <BackHandlerView hide={hide} handleBack={handleBack}>
+            <StatusBar backgroundColor={theme.variables.statusBarBgLight} barStyle={theme.variables.statusBarColorDark} />
             <ConfirmAddManufacturerNetwork visible={maoVisible} accept={addNetworkHandler.acceptManufacturerAsOwner} reject={addNetworkHandler.refuseManufacturerAsOwner} />
             <Step
               hide={hide}
