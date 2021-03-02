604907cec68e3b1e4739b164c13178bedc5b4b4c
Sami <dimkasami@gmail.com>
2021-03-02 17:31:12 +0100

    added missing data warning

--- src/views/main/LogScreen/index.js
+++ src/views/main/LogScreen/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
+import Text from '../../../components/Text';
 import GraphChart from './GraphChart';
 import ChartHeader from './ChartHeader';
 import LogComponent, { STATUS } from './LogComponent';
@@ -6,18 +7,22 @@ import { ActivityIndicator } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import useEntitySelector from 'wappsto-blanket/hooks/useEntitySelector';
 import equal from 'deep-equal';
+import { useTranslation, CapitalizeFirst } from '../../../translations';
 
 const LogScreen = React.memo(() => {
+  const { t } = useTranslation();
   const route = useRoute();
   const valueId = route.params?.id;
   const value = useEntitySelector('value', valueId);
-  const permission = value?.permission || '';
   const [ loading, setLoading ] = useState(false);
   const [ data, setData ] = useState({});
   const [ options, setOptions ] = useState();
   const [ values, setValues ] = useState([]);
   const prevOptions = useRef(options);
   const cachedData = useRef({});
+  const errors = useRef({});
+  const hasReport = value?.permission?.includes('r');
+  const hasControl = value?.permission?.includes('w');
 
   const onDone = useCallback((data) => {
     if(data.status === 'unmount'){
@@ -25,6 +30,9 @@ const LogScreen = React.memo(() => {
       return;
     } else {
       cachedData.current[data.type] = data;
+      if(data.status === STATUS.error){
+        errors.current[data.type] = true;
+      }
     }
     for(let key in cachedData.current){
       if(!cachedData.current[key]){
@@ -36,7 +44,7 @@ const LogScreen = React.memo(() => {
   }, []);
 
   useEffect(() => {
-    if(!options){
+    if(!options || (!hasReport && !hasControl)){
       return;
     }
     const rawOptions = {
@@ -55,26 +63,31 @@ const LogScreen = React.memo(() => {
     if(!rawOptions.start || !rawOptions.end || equal(rawOptions, prevOptions.current)){
       return;
     }
-    setLoading(true);
+    errors.current = {};
     cachedData.current = {};
     prevOptions.current = rawOptions;
+    setLoading(true);
     const values = [];
-    if(permission.includes('r')){
+    if(hasReport){
       cachedData.current.Report = null;
       values.push(<LogComponent key='read' id={valueId} options={rawOptions} onDone={onDone} stateType='Report' onlyNumber={!options.isList} />);
     }
-    if(permission.includes('w')){
+    if(hasControl){
       cachedData.current.Control = null;
       values.push(<LogComponent key='write' id={valueId} options={rawOptions} onDone={onDone} stateType='Control' onlyNumber={!options.isList} />);
     }
     setValues(values);
-  }, [options, valueId, permission, onDone]);
+  }, [options, valueId, hasReport, hasControl, onDone]);
 
   return (
     <>
       {values}
       <ChartHeader options={options} setOptions={setOptions}>
         {loading && <ActivityIndicator color='red' />}
+        {errors.current.Report && <Text color='error' content={CapitalizeFirst(t('failedToGetReportData'))}/>}
+        {!loading && !errors.current.Report && hasReport && !data.Report?.data.length && <Text color='warning' content={CapitalizeFirst(t('noReportDataWarning'))}/>}
+        {errors.current.Control && <Text color='error' content={CapitalizeFirst(t('failedToGetControlData'))}/>}
+        {!loading && !errors.current.Control && hasControl && !data.Control?.data.length && <Text color='warning' content={CapitalizeFirst(t('noControlDataWarning'))}/>}
         <GraphChart data={data} operation={options?.operation}/>
       </ChartHeader>
     </>
