ca5c8bf844f3137954a256c2db9e92a556b8d3bf
evelinamihailova <evelinamihailova@gmail.com>
2023-04-04 09:53:16 +0200

    reverted dayjs to momentjs in Timestamp due to fromNow() causing performance issues. getting timezone based timestamp takse around 20-30 ms per timestamp

--- src/components/Timestamp.js
+++ src/components/Timestamp.js
@@ -1,16 +1,18 @@
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import moment from 'moment/moment';
+// import dayjs from 'dayjs';
+// import relativeTime from 'dayjs/plugin/relativeTime';
 import React from 'react';
 import { useRefreshTimestamp } from 'wappsto-blanket';
 import Text from './Text';
 
-dayjs.extend(relativeTime);
+// dayjs.extend(relativeTime);
 
-const Timestamp = React.memo(({ timestamp }) => {
-  const dateString = dayjs(timestamp).fromNow();
+const Timestamp = ({ timestamp }) => {
+  // const dateString = dayjs(timestamp).fromNow();
+  const dateString = moment(timestamp).fromNow();
   useRefreshTimestamp(timestamp);
   return <Text color="secondary" size={12} content={dateString} />;
-});
+};
 
 Timestamp.displayName = 'Timestamp';
 export default Timestamp;
